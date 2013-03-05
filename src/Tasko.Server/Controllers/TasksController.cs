a1bfc561b66ecd09f65ddaa3e9456d2c126e451e
Christian Specht <christi@nspecht.de>
2013-03-05 23:32:35 +0100

    filter by category

--- src/Tasko.Server/Controllers/TasksController.cs
+++ src/Tasko.Server/Controllers/TasksController.cs
@@ -10,13 +10,18 @@ namespace Tasko.Server.Controllers
     {
         Task[] tasks = new Task[]
         {
-            new Task { Id = 1, Description = "task1" },
-            new Task { Id = 2, Description = "task2" }
+            new Task { Id = 1, Description = "task1", Categories= new List<string> { "CatA" } }, 
+            new Task { Id = 2, Description = "task2", Categories= new List<string> { "CatA", "CatB" } }            
         };
 
         public IEnumerable<Task> Get()
         {
             return tasks;
+        }
+
+        public IEnumerable<Task> Get(string category)
+        {
+            return tasks.Where(t => t.Categories.Any(c => c.Contains(category)));
         }
 
         public Task Get(int id)
--- src/Tasko.Server/Models/Task.cs
+++ src/Tasko.Server/Models/Task.cs
@@ -1,9 +1,11 @@
-﻿
+﻿using System.Collections.Generic;
+
 namespace Tasko.Server.Models
 {
     public class Task
     {
         public int Id { get; set; }
         public string Description { get; set; }
+        public List<string> Categories { get; set; }
     }
 }
