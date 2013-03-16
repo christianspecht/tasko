6d7db88f8246a2bcff5501e70926e31bf2eec76a
Christian Specht <christi@nspecht.de>
2013-03-16 20:14:46 +0100

    add test project

---
+++ src/Tasko.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tasko.Tests")]
+[assembly: AssemblyDescription("Tast assembly for Tasko")]
+[assembly: ComVisible(false)]
+[assembly: Guid("692f0892-630d-4b0f-b882-ad94a1fa6f4a")]
