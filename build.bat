41e1db88fb60833d73d3f97b1e5c71742c89f562
Christian Specht <christi@nspecht.de>
2013-03-16 21:17:26 +0100

    run tests from build script / batch file to open NUnit GUI / add dummy test

--- build.bat
+++ build.bat
@@ -4,6 +4,6 @@ path=%path%;%windir%\Microsoft.net\Framework\v4.0.30319
 
 cd %~dp0
 
-msbuild build.proj
+msbuild build.proj /p:RunTests="%1"
 
 pause
--- src/Tasko.Tests/Class1.cs
+++ src/Tasko.Tests/Class1.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using NUnit.Framework;
 
 namespace Tasko.Tests
 {
+    [TestFixture]
     public class Class1
     {
+        [Test]
+        public void DummyTest()
+        {
+            Assert.True(true);
+        }
     }
 }
