d72b3ffe4b1c998b5bf3e075a802493e1d939ec4
Christian Specht <christi@nspecht.de>
2013-06-09 22:38:36 +0200

    more tests / fix GetDto

--- src/Tasko.Tests/Integration/TasksControllerTests.cs
+++ src/Tasko.Tests/Integration/TasksControllerTests.cs
@@ -1,5 +1,7 @@
 ﻿using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Web.Http;
 using NUnit.Framework;
 using Raven.Client.Embedded;
 using Tasko.Server.Controllers;
@@ -34,8 +36,8 @@ namespace Tasko.Tests.Integration
         public TaskCreateDto GetDto(string description, string category)
         {
             var dto = new TaskCreateDto();
-            dto.Description = "foo";
-            dto.Category = "bar";
+            dto.Description = description;
+            dto.Category = category;
             return dto;
         }
 
@@ -75,6 +77,109 @@ namespace Tasko.Tests.Integration
 
                     Assert.That(task.Id > 0);
                     Assert.That(task.Description == "foo");
+                    Assert.That(task.Categories.First() == "bar");
+                }
+            }
+
+            [Test]
+            public void ReturnsCreatedOnSuccess()
+            {
+                using (var c = GetController())
+                {
+                    var dto = GetDto("foo", "bar");
+                    var resp = c.Post(dto);
+
+                    Assert.That(resp.StatusCode == HttpStatusCode.Created);
+                }
+            }
+
+            [Test]
+            public void ReturnsBadRequestWhenNoDtoIsPassed()
+            {
+                using (var c = GetController())
+                {
+                    var resp = c.Post(null);
+                    Assert.That(resp.StatusCode == HttpStatusCode.BadRequest);
+                }
+            }
+        }
+
+        [TestFixture]
+        public class Get : TasksControllerTests
+        {
+            [Test]
+            public void LoadsOneTaskById()
+            {
+                int taskId = 0;
+
+                using (var c = GetController())
+                {
+                    var dto = GetDto("foo", "bar");
+                    var resp = c.Post(dto);
+                    var task = resp.Content.ReadAsAsync<Task>().Result;
+                    taskId = task.Id;
+                    Assert.That(taskId != 0);
+                }
+
+                using (var c = GetController())
+                {
+                    var task = c.Get(taskId);
+
+                    Assert.That(task != null);
+                    Assert.That(task.Description == "foo");
+                    Assert.That(task.Categories.First() == "bar");
+                }
+            }
+
+            [Test]
+            public void ReturnsNotFoundWhenLoadingNonExistingTaskById()
+            {
+                using (var c = GetController())
+                {
+                    var ex = Assert.Throws<HttpResponseException>(() => c.Get(100));
+                    Assert.That(ex.Response.StatusCode == HttpStatusCode.NotFound);
+                }
+            }
+
+            [Test]
+            public void LoadsAllTasks()
+            {
+                using (var c = GetController())
+                {
+                    var dto = GetDto("foo1", "bar");
+                    c.Post(dto);
+                    dto = GetDto("foo2", "bar");
+                    c.Post(dto);
+                    dto = GetDto("foo3", "bar");
+                    c.Post(dto);
+                }
+
+                using (var c = GetController())
+                {
+                    var tasks = c.Get();
+                    Assert.That(tasks != null);
+                    Assert.That(tasks.Count() == 3);
+                }
+            }
+
+            [Test]
+            public void LoadTasksByCategory()
+            {
+                using (var c = GetController())
+                {
+                    var dto = GetDto("foo1", "cat1");
+                    c.Post(dto);
+                    dto = GetDto("foo2", "cat1");
+                    c.Post(dto);
+                    dto = GetDto("foo3", "cat2");
+                    c.Post(dto);
+                }
+
+                using (var c = GetController())
+                {
+                    var tasks = c.Get("cat1");
+                    Assert.That(tasks != null);
+                    Assert.That(tasks.Count() == 2);
                 }
             }
         }
