492716c4cfabc0c86ffbed814094415117db5eaf
Christian Specht <christi@nspecht.de>
2015-02-03 22:59:45 +0100

    create zip file (with version number)

--- build.bat
+++ build.bat
@@ -4,6 +4,8 @@ path=%path%;%programfiles(x86)%\MSBuild\12.0\Bin
 
 cd %~dp0
 
+call version-number.bat
+
 msbuild build.proj /p:RunTests="%1"
 
 pause
---
+++ version-number.bat
@@ -0,0 +1,4 @@
+@echo off
+
+rem The version number of the Tasko server. Increase for each release.
+set VersionNumber=1.0
